5bfd1c1e27c36239f39e5cc502afdede4c0cdeb9
hangaoke1 <188869009@qq.com>
2020-02-14 19:14:23 +0800

    feat: update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-const _ = require('lodash')
 const argPort = process.argv[2]
 const port = !!argPort ? +argPort : 9610
-const config = require('../config')
 
 app.engine('html', require('ejs').renderFile);
-app.use(bodyParser.json({limit: '2mb'}))
-app.use(bodyParser.urlencoded({limit: '2mb', extended: true}))
+app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.urlencoded({ limit: '2mb', extended: true }))
 app.use((req, resp, next) => {
     resp.header("Access-Control-Allow-Credentials", true)
     resp.header('Access-Control-Allow-Origin', '*')
@@ -17,19 +15,32 @@ app.use((req, resp, next) => {
 })
 
 app.use('/index', (req, res) => {
-	res.render("index.html",{ title:"主页" })
+    res.render("index.html", { title: "主页" })
 })
 
-app.use('/refreshToken', (req, res) => {
-	res.render("refreshToken.html",{ fingerprintUrl: config.serverUrl.fingerprint, title: "指纹同步" })
-})
+const whiteList = [
+    '952710000',
+    '952710001',
+    '952710002',
+    '952710003',
+]
 
-app.use('/refreshCustom', (req, res) => {
-	res.render("refreshCustom.html",{ title: "指纹同步" })
-})
-
-app.use('/refreshTokenUrl', (req, res) => {
-	res.render("refreshTokenUrl.html",{ title: "指纹同步" })
+app.use('/version', (req, res) => {
+    if (whiteList.includes(req.query.appkey)) {
+        res.json({
+            "flag": 1,
+            "commonUrl": "",
+            "version": "1.10.0"
+        }) 
+    } else {
+        res.json({
+            "flag": 1,
+            "commonUrl": "",
+            "androidUrl": "",
+            "iosUrl": "",
+            "version": "1.0.0"
+        })
+    }
 })
 
 app.listen(port, () => {
