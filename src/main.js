26ad2701af38757a2fc3ae6978d1a15ab8d04237
hangaoke1 <188869009@qq.com>
2020-06-17 11:07:19 +0800

    feat: update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser')
 const argPort = process.argv[2]
 const port = !!argPort ? +argPort : 9610
 
+const whiteList = [
+    '952710000',
+    '952710001',
+    '952710002',
+    '952710003',
+]
+
+const appidList = [
+    'wid04087891', // 爱伤客户【老叶】
+]
+
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.json({ limit: '2mb' }))
 app.use(bodyParser.urlencoded({ limit: '2mb', extended: true }))
@@ -18,12 +29,20 @@ app.use('/index', (req, res) => {
     res.render("index.html", { title: "主页" })
 })
 
-const whiteList = [
-    '952710000',
-    '952710001',
-    '952710002',
-    '952710003',
-]
+// 检测是否在app白名单
+app.use('/v', (req, res) => {
+    if (appidList.includes(req.query.appid)) {
+        res.json({
+            flag: 1,
+            data: true
+        }) 
+    } else {
+        res.json({
+            flag: 1,
+            data: false
+        })
+    }
+})
 
 app.use('/version', (req, res) => {
     if (whiteList.includes(req.query.appkey)) {
